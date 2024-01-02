96f6995b6b7ddd98178f2be492c48429bece2c15
Joshua Fisk <yodaboy@gmail.com>
2024-01-02 14:59:55 -0800

    generate board controller and views

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,12 @@
+class BoardsController < ApplicationController
+  def index
+    #page for generating new board
+  end
+
+  def show
+    @board = "just simple text for now thank you"
+  end
+
+  def all
+  end
+end
