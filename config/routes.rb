46d522277318cceb7b8b5b136e48e2ebdd036287
Joshua Fisk <yodaboy@gmail.com>
2024-01-02 16:19:21 -0800

    Correcting simple routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get '/boards',  to: "boards#index"
-  get '/boards/:id', to: "boards#show"
-  get '/boards/new', to: 'boards#new'
+  root "boards#index"
 
+  resources :boards, only: [:index, :show, :new]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "boards#index"
 end
