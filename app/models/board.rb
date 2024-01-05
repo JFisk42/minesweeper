7a023a3127abe20ca8afba7d33de2921d7238dd4
Joshua Fisk <yodaboy@gmail.com>
2024-01-04 18:14:41 -0800

    improved model validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,11 +1,14 @@
 class Board < ApplicationRecord
   validates :name, presence: true, length: { maximum: 1000 }
-  validates :email, presence: true, length: { maximum: 100 } # TODO add email validation 
+  validates :email, presence: true, length: { maximum: 100 } 
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :height, :width, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :mine_count, presence: true
   validate :mine_count_size
 
   def mine_count_size
+    return unless mine_count
+
     if mine_count < 1 || mine_count > self.height * self.width
       errors.add(:mine_count, 'The number of mines must be a positive integer smaller than the board area (height * width)')
     end
